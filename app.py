7653dcf714a1e6ef08dcbfa91af72447891d4605
bernardo <bernardo.dauria@uc3m.es>
2020-03-18 14:57:31 +0100

    example completed

--- app.py
+++ app.py
@@ -6,13 +6,15 @@ import dash_bootstrap_components as dbc # import the library
 import dash_core_components as dcc
 import dash_html_components as html
 import plotly.graph_objs as go
-from dash.dependencies import Input, Output
+import dash_table
+from dash.dependencies import Input, Output, State
+import json
 
 import pandas as pd
-df_url = 'https://forge.scilab.org/index.php/p/rdataset/source/file/master/csv/ggplot2/msleep.csv'
-df = pd.read_csv(df_url)
-df_vore = df['vore'].dropna().sort_values().unique()
-opt_vore = [{'label': x + 'vore', 'value': x} for x in df_vore]
+db_url = 'https://forge.scilab.org/index.php/p/rdataset/source/file/master/csv/ggplot2/msleep.csv'
+db = pd.read_csv(db_url)
+db_vore = db['vore'].dropna().sort_values().unique()
+opt_vore = [{'label': x + 'vore', 'value': x} for x in db_vore]
 
 def generate_table(dataframe, max_rows=10):
     return html.Table(
@@ -34,7 +36,7 @@ app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 app.title = 'My First Dash App'
 
 colors = {
-    'background': '#111111',
+    'background': 'lightgray',
     'text': '#7FDBFF'
 }
 
@@ -53,30 +55,26 @@ app.layout = html.Div(style={"backgroundColor": colors['background'], 'color': c
                     'color': colors['text']
                     }),
             dcc.Markdown(markdown_text),
-            
-            html.Div(id='my-div',
-             style={
-                 'background' : 'yellow',
-                 'color' : 'blue'
-             }),
+            html.Div(id='my-div', style={'display': 'none'}),
     
             dcc.Graph(id='my-graph'),
+            dcc.Graph(id='my-box-plot'),
             
-            # generate_table(df)
+            # generate_table(db)
             
             html.Div([
                 html.Label('Dropdown'),
                 dcc.Dropdown(
                         id='my-dropdown',
                         options=opt_vore,
-                        value=df_vore[0]
+                        value=db_vore[0]
                 ),
                 
                 html.Label('Multi-Select Dropdown'),
                 dcc.Dropdown(
                     id='my-multi-dropdown',
                     options=opt_vore,
-                    value=df_vore[0],
+                    value=db_vore[0],
                     multi=True
                 ),
             
@@ -104,29 +102,50 @@ app.layout = html.Div(style={"backgroundColor": colors['background'], 'color': c
                 dcc.Input(value='MTL', type='text'),
             
                 html.Label('Slider'),
-                dcc.Slider(
-                    min=0,
-                    max=9,
-                    marks={i: 'Label {}'.format(i) if i == 1 else str(i) for i in range(1, 6)},
-                    value=5,
+                html.Div(
+                    dcc.RangeSlider(
+                        id='my-slider',
+                        step=0.1,
+                        min=min(db['sleep_total']),
+                        max=max(db['sleep_total'])
+                    ),
+                    style={
+                        'margin': '10%'
+                    }
                 ),
-            ], style={'columnCount': 2})
-    
-        
+                html.Button('Update filter', id='my-button')
+
+            ], style={'columnCount': 2}),
+
+            dash_table.DataTable(
+                id='my-table',
+                columns=[{"name": i, "id": i} for i in db.columns]
+            )
         ])
                 
 @app.callback(
     Output('my-div', 'children'),
-    [Input('my-dropdown', 'value')]
-)
-def update_output_div(input_value):
-    return 'You\'ve entered "{}"'.format(input_value)
+    [Input('my-button', 'n_clicks')],
+    [State('my-slider', 'value')])
+def update_data(n_clicks, slider_range):
+    if (slider_range and len(slider_range) == 2):
+        l, h = slider_range
+    else :
+        l, h = min(db['sleep_total']), max(db['sleep_total']);
+    df = db[db['sleep_total'].between(l,h)].to_json(orient='split', date_format='iso')
+    return json.dumps(df)
 
 @app.callback(
-    Output('my-graph', 'figure'),
-    [Input('my-multi-dropdown', 'value')]
+    [Output('my-graph', 'figure'),
+     Output('my-box-plot', 'figure')],
+    [Input('my-div', 'children'),
+     Input('my-multi-dropdown', 'value')]
 )
-def update_output_graph(input_value):
+def update_output_graph(data, input_value):
+    if data is None:
+        return {}, {}
+    dataset = json.loads(data)
+    df = pd.read_json(dataset, orient='split')
     return  {
                 'data': [
                     go.Scatter(
@@ -140,16 +159,60 @@ def update_output_graph(input_value):
                             'line': {'width': 0.5, 'color': 'white'}
                         },
                         name=i
-                    ) for i in df_vore
+                    ) for i in db_vore
                 ],
                 'layout': go.Layout(
                     xaxis={'type': 'log', 'title': 'Body weight (kg)'},
                     yaxis={'title': 'Total daily sleep time (hr)'},
                     margin={'l': 40, 'b': 40, 't': 10, 'r': 10},
                     legend={'x': 0, 'y': 1},
-                    hovermode='closest'
+                    hovermode='closest',
+                    dragmode='lasso'
                 )
+            },  {
+                'data': [ go.Box(
+                            y= df[df['vore'] == i]['sleep_total'],
+                            name= i + 'vore'
+                        ) if i in input_value else {}
+                          for i in db_vore ]
             }
+
+@app.callback(
+    [Output('my-slider', 'min'), 
+     Output('my-slider', 'max'), 
+     Output('my-slider', 'value'), 
+     Output('my-slider', 'marks')],
+    [Input('my-multi-dropdown', 'value')]
+)
+def update_slider(input_value):
+    def round(x):
+        return int(x) if x % 0.1 < 0.1 else x
+
+    s = pd.Series(input_value, name='vore')
+    data = db[db.vore.isin(s)]['sleep_total'] 
+
+    min = round(data.min())
+    max = round(data.max())
+    mean = round(data.mean())
+    low = round((min + mean)/2)
+    high = round((max + mean) / 2)
+    marks = {min: {'label': str(min), 'style': {'color': '#77b0b1'}},
+             max: {'label': str(max), 'style': {'color': '#77b0b1'}}}
+    return min, max,  [low, high], marks 
+
+@app.callback(
+    Output('my-table', 'data'),
+    [Input('my-graph', 'selectedData')])
+def display_selected_data(selected_data):
+    if selected_data is None or len(selected_data) == 0:
+        return []
+
+    points = selected_data['points']
+    if len(points) == 0:
+        return []
+
+    names = [x['text'] for x in points]
+    return db[db['name'].isin(names)].to_dict("rows")
 
 if __name__ == '__main__':
     app.run_server(port=5054, debug=True) # debug=True to enable hot reload
